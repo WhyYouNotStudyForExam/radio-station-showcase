411df213ecd46d07d3277aabaa08620dbdd4dea8
Micha <mischaschev@web.de>
2024-03-31 19:30:35 +0200

    Implements NavGraph

---
+++ app/src/main/java/com/example/radiostationshowcase/ui/StationDetailsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.radiostationshowcase.ui
+
+import androidx.fragment.app.Fragment
+import com.example.radiostationshowcase.R
+
+class StationDetailsFragment : Fragment(R.layout.fragment_station_details) {
+}
---
+++ app/src/main/java/com/example/radiostationshowcase/ui/TopStationsFragment.kt
@@ -0,0 +1,8 @@
+package com.example.radiostationshowcase.ui
+
+import androidx.fragment.app.Fragment
+import com.example.radiostationshowcase.R
+
+class TopStationsFragment : Fragment(R.layout.fragment_top_stations) {
+
+}
